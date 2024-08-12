bd0051117c17f82a80583f1be9b8b324a124c91b
최지은 <jechoi@exosp.com>
2024-08-12 17:09:18 +0900

    Initial Commit

---
+++ ToyProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ToyProject
+//
+//  Created by jieunchoi on 8/12/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
